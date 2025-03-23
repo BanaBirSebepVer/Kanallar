762d6ca89e2e624d63bfba1a3d85de56d31c022e
canbekcan <can@canbekcan.com>
2025-03-23 12:06:17 +0200

    Dosyalar yüklendi

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addVideo(videoId) {
     const videoContainer = document.createElement('div');
     videoContainer.className = 'video-container';
     videoContainer.innerHTML = `
+        <button class="remove-btn" onclick="removeVideo(this.parentElement)">×</button>
         <iframe
             src="https://www.youtube.com/embed/${videoId}"
             frameborder="0"
@@ -23,6 +24,14 @@ function addVideo(videoId) {
     videoGrid.appendChild(videoContainer);
 }
 
+function removeVideo(container) {
+    const index = videos.indexOf(container);
+    if (index > -1) {
+        videos.splice(index, 1);
+        container.remove();
+    }
+}
+
 addChannelForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const videoUrl = document.getElementById('videoUrl').value;
@@ -32,6 +41,6 @@ addChannelForm.addEventListener('submit', (e) => {
         addVideo(videoId);
         document.getElementById('videoUrl').value = '';
     } else {
-        alert('Please enter a valid YouTube URL');
+        alert('Lütfen geçerli bir YouTube URL\'si girin');
     }
 });
